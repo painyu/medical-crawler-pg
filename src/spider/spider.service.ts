a39f7f1ce3cc1886bc959776f125f64623f43213
杨晓勇 <yangjackyong@163.com>
2024-06-25 13:21:13 +0800

    提交爬虫代码

--- src/spider/spider.service.ts
+++ src/spider/spider.service.ts
@@ -17,6 +17,9 @@ export class SpiderService {
 
 
   async findSpiderListPage(page: number, limit: number, keyword: string): Promise<ResultData> {
+    if (keyword === undefined) {
+      return ResultData.fail(500, "关键字不能为空");
+    }
     let spiderList = await this.spiderRepository.createQueryBuilder('spider_website')
       .where("keywords LIKE  :keyword", { keyword: `%${keyword}%` })
       .skip(page)
