c0c9311c33578c3ff8c15ec223a4d2d95516dec0
杨晓勇 <yangjackyong@163.com>
2024-06-26 09:59:41 +0800

    提交认证代码

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AllowAnon } from './decorators/allow-anon.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @AllowAnon()
   @Get()
   getHello(): string {
     return this.appService.getHello();
